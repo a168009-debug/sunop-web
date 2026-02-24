d80d5d0912e21786b8d113b2a1124f4c8e1767cb
a168009-debug <a168009@gmail.com>
2026-02-24 19:46:32 +0700

    Fix syntax error

--- netlify/functions/bazi_ai.js
+++ netlify/functions/bazi_ai.js
@@ -6,8 +6,9 @@ const DEEP_CONFIG = { model: "gpt-4o", max_tokens: 800, timeout: 15000 };
 function withTimeout(promise, ms) {
   return Promise.race([
     promise,
-    new Promise((_, reject) => setTimeout(() => reject(new Error("timeout")), ms)
+    new Promise((_, reject) => setTimeout(() => reject(new Error("timeout")), ms))
   ]);
+}
 }
 
 function calculateBazi(year, month, day, hourBranch) {
